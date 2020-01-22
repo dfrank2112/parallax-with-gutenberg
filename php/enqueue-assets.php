1fd9028f367d53699520c565b4046ff8fa159728
Josh Baker <me@joshbaker.codes>
2020-01-21 22:16:03 -0500

    better comments

--- php/enqueue-assets.php
+++ php/enqueue-assets.php
@@ -7,7 +7,7 @@ function jab_block_assets()
     // Styles
     wp_enqueue_style('jab-block-save-style', $style_path, '');
 
-    // React
+    // React JS
     wp_enqueue_script('jab-react-js', $react_path, ['wp-editor'], null, true);
 }
 add_action('enqueue_block_assets', 'jab_block_assets');
@@ -17,10 +17,10 @@ function jab_editor_assets()
     $blocks_path = plugin_dir_url( __FILE__ ) . '../build/js/blocks.min.js';
     $style_path = plugin_dir_url( __FILE__ ) . '../build/css/blocks-edit.css';
 
-    // Register our block script with WordPress
+    // Block JS
     wp_enqueue_script('jab-block-js', $blocks_path, [ 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ], true);
 
-    // Register our block's editor-specific CSS
+    // Edit CSS
     wp_enqueue_style('jab-block-editor-style', $style_path, [ 'wp-edit-blocks' ]);
 }
 add_action('enqueue_block_editor_assets', 'jab_editor_assets');
